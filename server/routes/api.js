e613d49ae0604b9ea9de180d166bb43ea32f056b
Gabe Rodriguez <grod220@gmail.com>
2017-01-25 15:41:57 +0100

    facebook api pulling correctly

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const rp = require('request-promise');
 const router = express.Router();
+
 
 // Facebook verification
 router.get('/webhook', (req, res) => {
@@ -10,10 +12,29 @@ router.get('/webhook', (req, res) => {
   }
 });
 
+router.get('/fbtest', (req, res) => {
+  let options = {
+    uri: `https://graph.facebook.com/v2.8/790534394301792/feed?fields=permalink_url&access_token=${process.env.APPID}|${process.env.APPSECRET}`,
+    json: true // Automatically parses the JSON string in the response
+  };
+
+  rp(options)
+      .then(function (response) {
+        let mostRecentPostURL = response.data[0].permalink_url;
+        res.send(mostRecentPostURL)
+        // send to Database
+      })
+      .catch(function (err) {
+        res.send(err)
+          console.log(err.body);
+      });
+});
+
 // Handle incoming FB webhook verification
 router.post('/webhook', (req, res) => {
   console.log(req.body);
   console.log(req.body.entry[0].changes);
+  console.log('**************************');
 
   // Send get request to FB for permalink
 
