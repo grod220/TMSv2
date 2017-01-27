54efe9915c009f82c4d86f34ec15af598b490f53
Puneet <sachdeva.puneetsachdeva.puneet@gmail.com>
2017-01-27 17:41:34 +0530

    Added Header Component

---
+++ src/app/shared/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+      
+@Component({
+	moduleId: module.id,
+	selector: 'header-block',
+	templateUrl: './header.component.html'
+})
+export class HeaderComponent  {
+	
+
+	
+}
