3633575fe0d66f3d6b00b0bb35e8f0f31e277c7a
Puneet <sachdeva.puneetsachdeva.puneet@gmail.com>
2017-01-27 17:41:21 +0530

    Added Socail Bar Component

---
+++ src/app/shared/social-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+      
+@Component({
+	moduleId: module.id,
+	selector: 'socail-bar-block',
+	templateUrl: './socail-bar.component.html'
+})
+export class SocailBarComponent  {
+	
+
+	
+}
