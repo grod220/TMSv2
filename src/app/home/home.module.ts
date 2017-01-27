34cd365f3b0344151ba2770faa615987e3447c12
Puneet <sachdeva.puneetsachdeva.puneet@gmail.com>
2017-01-27 18:24:15 +0530

    updated

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { HomeComponent }   from './home.component';
 import { HomeRoutingModule }   from './home-routing.module';
 
+
 @NgModule({
       imports: [HomeRoutingModule],
       exports: [],
