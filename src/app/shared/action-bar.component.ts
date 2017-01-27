02368bdfe36237993bfa8e788ed24e6df3ab9794
Puneet <sachdeva.puneetsachdeva.puneet@gmail.com>
2017-01-27 17:25:36 +0530

    Added Action Bar component

---
+++ src/app/shared/action-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+      
+@Component({
+	moduleId: module.id,
+	selector: 'action-bar-block',
+	templateUrl: './action-bar.component.html'
+})
+export class ActionBarComponent  {
+	
+
+	
+}
