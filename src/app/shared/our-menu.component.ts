208caf6526bc2ff0bc1ed8bdad35cf32dc6ef8b1
Puneet <sachdeva.puneetsachdeva.puneet@gmail>
2017-03-03 13:13:09 +0530

    Add Our Menu Bar style and include file for Our Menu Component

--- src/app/shared/our-menu.component.ts
+++ src/app/shared/our-menu.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
 	moduleId: module.id,
 	selector: 'our-menu-block',
-	templateUrl: './our-menu.component.html'
+	templateUrl: './our-menu.component.html',
+	styleUrls: ['./our-menu.component.css']
 })
 export class OurMenuComponent  {
 	
