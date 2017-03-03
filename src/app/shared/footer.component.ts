c473d5c54d9d02637d6f77854f5955fd1268829b
Puneet <sachdeva.puneetsachdeva.puneet@gmail>
2017-03-03 12:43:31 +0530

    Add Footer style and include file for Footer Component

--- src/app/shared/footer.component.ts
+++ src/app/shared/footer.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
 	moduleId: module.id,
 	selector: 'footer-block',
-	templateUrl: './footer.component.html'
+	templateUrl: './footer.component.html',
+	styleUrls: ['./footer.component.css']
 })
 export class FooterComponent  {
 	
