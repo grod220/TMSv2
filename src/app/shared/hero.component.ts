3674bcbaf2eb9f7ae744afd59c5be3d82775cc22
Ravinder SIngh <r.singh.negi85@gmail.com>
2017-03-03 11:40:19 +0530

    put hero style file’s path on hero component

--- src/app/shared/hero.component.ts
+++ src/app/shared/hero.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
 	moduleId: module.id,
 	selector: 'hero-block',
-	templateUrl: './hero.component.html'
+	templateUrl: './hero.component.html',
+	styleUrls: ['app/shared/hero.component.css']
 })
 export class HeroComponent  {
 	
