62149e6d6a708673c337fd4f4400071f515a7fc1
Puneet <sachdeva.puneetsachdeva.puneet@gmail>
2017-03-03 12:42:32 +0530

    include file for style

--- src/app/shared/action-bar.component.ts
+++ src/app/shared/action-bar.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
 	moduleId: module.id,
 	selector: 'action-bar-block',
-	templateUrl: './action-bar.component.html'
+	templateUrl: './action-bar.component.html',
+	styleUrls: ['./action-bar.component.css']
 })
 export class ActionBarComponent  {
 	
