5128599dcafbf98ffe127370dc9d8228bd615dfb
Puneet <sachdeva.puneetsachdeva.puneet@gmail>
2017-03-03 13:17:06 +0530

    Add Testimonials style and include file for Testimonials Component

--- src/app/shared/testimonials.component.ts
+++ src/app/shared/testimonials.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
 	moduleId: module.id,
 	selector: 'testimonials-block',
-	templateUrl: './testimonials.component.html'
+	templateUrl: './testimonials.component.html',
+	styleUrls: ['./testimonials.component.css']
+	
 })
 export class TestimonialComponent  {
 	
