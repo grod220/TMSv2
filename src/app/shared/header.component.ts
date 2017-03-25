7c093306824446b55ce405db23c53ad73b88c070
denjamal <devmalimon@gmail.com>
2017-03-25 16:32:17 +0200

    Small refactoring for header.ts file

--- src/app/shared/header.component.ts
+++ src/app/shared/header.component.ts
@@ -9,25 +9,42 @@ declare var $: any;
 	styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+	adress: string = "7325 Lake Underhill Road 407-270-6505";
 	ngOnInit(): void {
+		
+		this.toggleClickHandler();
+		this.appendAdditionalHTML();
+		
+		$(".menu-close").click(this.triggerMenuClick);
+		$(".navbar-collapse li a").click(this.triggerMenuClick);
+		
+	}
+
+	private toggleClickHandler() {
 		$("button").on("click", function () {
 			let menu = $(".navbar-collapse")
 			let isCollapsed = menu.attr("aria-expanded");
 			switch (isCollapsed) {
 				case "true":
 					$(".navbar-collapse").removeClass("slideShow");
-					$(".navbar-collapse").addClass("slideUp"); break;
+					$(".navbar-collapse").addClass("slideUp");
+					$(".eclipse").remove(); break;
 				case undefined:
 				case "false":
 					$(".navbar-collapse").removeClass("slideUp");
-					$(".navbar-collapse").addClass("slideShow");break;
+					$(".navbar-collapse").addClass("slideShow");
+					$("<div>").addClass("eclipse").appendTo("html"); break;
 			}
 		});
+	}
 
+	private appendAdditionalHTML (){
 		$("<div>").html("x").addClass("menu-close").appendTo(".navbar-collapse");
-		$("<div>").html("<p>7325 Lake Underhill Road 407-270-6505</p>").addClass("menu-address").appendTo(".navbar-collapse");
-		$(".menu-close").click(function(){
-			$(".navbar-toggle").trigger("click");
-		});
+		$("<div>").html(`<p>${this.adress}</p>`).addClass("menu-address").appendTo(".navbar-collapse");
+	}
+
+	private triggerMenuClick(){
+		$(".navbar-toggle").trigger("click");
 	}
 }
