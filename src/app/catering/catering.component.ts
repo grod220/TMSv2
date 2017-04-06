4fe8c91e81d434994d18c7c746b5a9016e302553
Gabe Rodriguez <grod220@gmail.com>
2017-04-06 08:28:39 -0700

    adding catering hero

--- src/app/catering/catering.component.ts
+++ src/app/catering/catering.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./catering.component.css']
 })
 export class CateringComponent implements OnInit {
-
+  headlineImage: string = '/assets/images/cateringhero.jpg';
   constructor() { }
 
   ngOnInit() {
